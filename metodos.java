c42951952b4318b2aa6aabc7192ad14071e25b7c
Emilmrk <93278758+Emilmrk@users.noreply.github.com>
2023-05-28 13:38:42 -0500

    Create metodos.java

---
+++ metodos.java
@@ -0,0 +1,9 @@
+public class metodos {
+	//metodo basico
+	public static void main(String[]args) {
+		miMetodo();
+	}
+	public static void miMetodo() {
+		System.out.println("este es un metodo");
+	}
+}
