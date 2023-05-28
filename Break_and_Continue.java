096ba3fa71d5bf5364a6dacebb50dcb25378a575
Emilmrk <93278758+Emilmrk@users.noreply.github.com>
2023-05-28 13:27:51 -0500

    uso simple del break y continue en java

---
+++ Break_and_Continue.java
@@ -0,0 +1,10 @@
+public class Break_and_Continue {
+	public static void main(String[]args) {
+		for (int i = 0; i < 17; i++) {
+			if(i==7) continue;
+			else if(i==10) break;
+			 System.out.println(i);
+		}
+		
+	}
+}
